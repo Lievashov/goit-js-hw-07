88f006ce14f1ad7510e5c484201f22d8041412b0
Ihor Lievashov <levashov.i.v.85@gmail.com>
2021-03-14 22:48:46 +0200

    Update task-4.js
    Изменил task-4

--- js/task-4.js
+++ js/task-4.js
@@ -4,39 +4,19 @@
 // Создай функции increment и decrement для увеличения и уменьшения значения счетчика
 // Добавь слушатели кликов на кнопки, вызовы функций и обновление интерфейса
 
-const addListenerBtnRef = document.querySelector('.add');
-const listenerBtnRef = document.querySelector('.listener');
-const deleteListenerBtnRef = document.querySelector('.delete');
-const counterRef = document.querySelector('#counter');
-let valueRef = document.querySelector('#value');
+const resultSpan = document.querySelector('#value');
+const decrementBtnEl = document.querySelector('button[data-action="decrement"]');
+const incrementBtnEl = document.querySelector('button[data-action="increment"]');
 
-const decrementBtn = document.querySelector('[data-action="decrement"]');
-const incrementBtn = document.querySelector('[data-action="increment"]');
+decrementBtnEl.addEventListener('click', decrement);
+incrementBtnEl.addEventListener('click', increment);
 
-valueRef.textContent = 15;
+function decrement() {
+  const  counterValue = Number(resultSpan.textContent)
+    resultSpan.textContent = counterValue - 1
+}
 
-console.log(valueRef.textContent);
-console.dir(valueRef.textContent);
-
-const doStuffOnClick = () => {
-  console.log('hi');
-};
-
-const decrement = () => {
-  if (valueRef.textContent > 0) {
-    valueRef.textContent = valueRef.textContent - 1;
-  }
-};
-const increment = () => {
-  valueRef.textContent = Number(valueRef.textContent) + 1;
-};
-addListenerBtnRef.addEventListener('click', () => {
-  listenerBtnRef.addEventListener('click', doStuffOnClick);
-});
-
-deleteListenerBtnRef.addEventListener('click', () => {
-  listenerBtnRef.removeEventListener('click', doStuffOnClick);
-});
-
-decrementBtn.addEventListener('click', decrement);
-incrementBtn.addEventListener('click', increment);
+function increment() {
+  const  counterValue = Number(resultSpan.textContent)
+    resultSpan.textContent = counterValue + 1
+}
